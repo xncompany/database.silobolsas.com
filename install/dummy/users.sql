134ff5e7f39dbb4fc07a3325cd1aaa63f58dfbe0
XN Developer <gonzalo+001@umewin.com>
2018-04-02 17:21:33 +0000

    Adding type of users

--- install/dummy/users.sql
+++ install/dummy/users.sql
@@ -4,9 +4,13 @@ USE `silobolsas`;
 
 # Password 00e8579911e947913fa6a5f5cf60abe7 is equivalent to 'xncompany'
 
-INSERT INTO `users` (`id`, `email`, `password`, `active`, `created_at`) VALUES
-    (1, 'lverardo@xncompany.com', '00e8579911e947913fa6a5f5cf60abe7', 1, '2017-01-09 01:55:12');
+INSERT INTO `users` (`id`, `email`, `password`, `active`, `user_type`, `created_at`) VALUES
+    (1, 'lverardo@xncompany.com', '00e8579911e947913fa6a5f5cf60abe7', 1, 1, '2017-01-09 01:55:12'),
+    (2, 'ariel@xncompany.com', '00e8579911e947913fa6a5f5cf60abe7', 1, 2, '2017-01-09 01:55:12');
 
+INSERT INTO `user_types` (`id`, `description`) VALUES
+    (1, 'admin'),
+    (2, 'read');
 
 INSERT INTO `user_attributes` (`id`, `description`) VALUES
     (1, 'fullname'),
@@ -17,4 +21,7 @@ INSERT INTO `user_attributes` (`id`, `description`) VALUES
 INSERT INTO `user_attribute_values` (`id`, `user`, `user_attribute`, `description`) VALUES
 	(null, 1, 1, 'Luciano Verardo'),
 	(null, 1, 2, 'Las Gaviotas'),
-	(null, 1, 3, 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKpAAAAJDJiZGE4YzhmLTVhNDQtNDY2ZC1hNGJiLTQ1MzA0MGZmNGVhMw.jpg');
+	(null, 1, 3, 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKpAAAAJDJiZGE4YzhmLTVhNDQtNDY2ZC1hNGJiLTQ1MzA0MGZmNGVhMw.jpg'),
+	(null, 2, 1, 'Ariel Scaliter'),
+	(null, 2, 2, 'Campo 3 de Febrero'),
+	(null, 2, 3, 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAL1AAAAJDg5YmJjMzMyLTE5OWQtNGQ1NS1hYTVkLTY1M2M4NmRmYTEwNQ.jpg');
--- install/tables/users.sql
+++ install/tables/users.sql
@@ -9,11 +9,22 @@ CREATE TABLE `users` (
  `email` varchar(128) NOT NULL,
  `password` varchar(32) NOT NULL,
  `active` tinyint(1) unsigned NOT NULL,
+ `user_type` tinyint(10) NOT NULL,
  `created_at` datetime DEFAULT NULL,
 
  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+# ================================================================================================
+
+DROP TABLE IF EXISTS `user_types`;
+
+CREATE TABLE `user_types` (
+ `id` tinyint(10) unsigned NOT NULL AUTO_INCREMENT,
+ `description` varchar(10) NOT NULL,
+
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # ================================================================================================
 
