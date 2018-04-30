fa83d3d7f8e0f6d2d01a44297fde8fc2a3e62680
XN Developer <xn-developer>
2018-04-30 23:59:36 +0000

    Lands based on Organizations

--- install/dummy/lands.sql
+++ install/dummy/lands.sql
@@ -2,7 +2,7 @@ USE `silobolsas`;
 
 # ================================================================================================
 
-INSERT INTO `lands` (`id`, `user`, `description`, `active`, `created_at`) VALUES
-	(null, 1, 'Las Gaviotas', 1, '2018-02-21 09:23:11'),
-	(null, 1, 'La Eliana', 1, '2018-02-21 19:13:25'),
-	(null, 2, 'El Payé', 1, '2018-03-11 21:23:54');
+INSERT INTO `lands` (`id`, `user`, `organization`, `description`, `active`, `created_at`) VALUES
+	(null, 1, 1, 'Las Gaviotas', 1, '2018-02-21 09:23:11'),
+	(null, 1, 1, 'La Eliana', 1, '2018-02-21 19:13:25'),
+	(null, 2, 1, 'El Payé', 1, '2018-03-11 21:23:54');
--- install/tables/lands.sql
+++ install/tables/lands.sql
@@ -7,12 +7,14 @@ DROP TABLE IF EXISTS `lands`;
 CREATE TABLE `lands` (
  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
  `user` bigint(20) NOT NULL,
+ `organization` int(10) NOT NULL,
  `description` varchar(128) NOT NULL,
  `active` tinyint(1) unsigned NOT NULL,
  `created_at` datetime DEFAULT NULL,
 
  PRIMARY KEY (`id`),
  KEY `user` (`user`),
+ KEY `organization` (`organization`),
  KEY `active` (`active`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
